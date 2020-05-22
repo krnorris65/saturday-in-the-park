d532fdfeee635354ffa7ccd6094332aaca59d818
Kristen Norris <krnorris65@gmail.com>
2020-05-22 11:42:27 -0500

    fixed itinerary item

--- src/components/itinerary/ItineraryItem.js
+++ src/components/itinerary/ItineraryItem.js
@@ -11,18 +11,3 @@ const ItineraryItem = props => {
 }
 
 export default ItineraryItem
-
-// {
-//     "id": 3,
-//     "url": "http://localhost:8000/itineraries/3",
-//     "attraction": {
-//         "url": "http://localhost:8000/attractions/7",
-//         "name": "Rider of Stars",
-//         "area": {
-//             "url": "http://localhost:8000/parkareas/3",
-//             "name": "Kennywood Speedway",
-//             "theme": "Planes, Trains, and Automobiles"
-//         }
-//     },
-//     "starttime": 8
-// }
